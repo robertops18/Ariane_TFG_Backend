12d1d52f6d32548a4f24f5b54ea64caa154ee6a7
robertops18 <robertops1818@gmail.com>
2019-05-24 18:29:04 +0200

    use statement

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,6 +3,7 @@
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Oh\GoogleMapFormTypeBundle\OhGoogleMapFormTypeBundle;
 
 class AppKernel extends Kernel
 {
