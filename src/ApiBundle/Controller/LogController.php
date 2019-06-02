5d79dc1205fe9073490dbaab6fa85274ec899180
robertops18 <robertops1818@gmail.com>
2019-06-02 21:06:22 +0200

    createLog API method

---
+++ src/ApiBundle/Controller/LogController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robertoperez
+ * Date: 2019-04-15
+ * Time: 17:43
+ */
+
+namespace ApiBundle\Controller;
+
+
+use AppBundle\Entity\Answer;
+use AppBundle\Entity\Log;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Swagger\Annotations as SWG;
+use FOS\RestBundle\Controller\FOSRestController;
+
+class LogController extends FOSRestController
+{
+    /** Creates an answer for the related task.
+     *
+     * @Route("/{taskId}",methods={"POST"})
+     * @SWG\Response(
+     *     response=200,
+     *     description="Log for task created"
+     * )
+     * @SWG\Response(
+     *     response=400,
+     *     description="Error creating the log"
+     * )
+     * @SWG\Parameter(
+     *       name="data",
+     *       in="body",
+     *       description="Action performed",
+     *       required=true,
+     *              @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(property="action", type="string")
+     *          )
+     *     )
+     * @SWG\Tag(name="Logs")
+     * @Security(name="Bearer")
+     */
+    public function createLogAction(Request $request, $taskId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $task = $em->getRepository('AppBundle:Task')->find($taskId);
+        $user = $this->getUser();
+
+        if (!$task) {
+            $view = $this->view(['error' => 'This task does not exist'], Response::HTTP_NOT_FOUND);
+            return $this->handleView($view);
+        }
+
+        $action = $request->get('action');
+
+        $log = new Log();
+        $log->setAction($action);
+        $log->setStudent($user);
+        $log->setTask($task);
+        $log->setFieldActivity($task->getFieldActivity());
+
+        $em->persist($log);
+        $em->flush();
+
+        $view = $this->view(['action' => 'Action correctly saved'], Response::HTTP_OK);
+        return $this->handleView($view);
+    }
+}
